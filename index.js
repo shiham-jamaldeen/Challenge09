cb405d016bb3593e861c76b2f0a23e5f8821fdea
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2022-11-06 21:03:48 +1030

    added function to request prompt from user and mark down template with bugs

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+let questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log(err) : console.log("success")
+  );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is the title of your project? (Required)",
+        name: "projectTitle",
+      },
+      {
+        type: "input",
+        message: "What is the description of your project?",
+        name: "projectDescription",
+      },
+      {
+        type: "input",
+        message: "What is your name? (Required)",
+        name: "authorName",
+      },
+      {
+        type: "input",
+        message: "What is your Github handle? (Required)",
+        name: "gitHubHandle",
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "emailAddress",
+      },
+    ])
+    .then((answer) => {
+      //print input to
+
+      let data = generateMarkdown(
+        answer.projectTitle
+        // answer.projectDescription,
+        // answer.authorName,
+        // answer.gitHubHandle,
+        // answer.emailAddress
+      );
+      //console.timeLog(data);
+      const fileName = "./utils/ReadMe-generated.md";
+      writeToFile(fileName, data);
+      //console.log("Success! Your data has been recorded: " + fileName);
+    });
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,15 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
-
+//![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+// if (license === "GNU") {
+//   const licenseURL = "https://img.shields.io/badge/license-GNU-brightgreen";
+// } else if (license === "") {
+//   const licenseURL = "";
+// } else {
+//   const licenseURL = "";
+// }
+// return licenseURL;
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {}
@@ -11,10 +19,58 @@ function renderLicenseLink(license) {}
 function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(answer) {
+  return `
+  
+  # ${answer.projectTitle}`;
 }
 
 module.exports = generateMarkdown;
+
+//If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+//Provide instructions and examples for use. Include screenshots as needed.
+
+//To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+//```md
+//![alt text](assets/images/screenshot.png)
+//```
+
+// answer.projectTitle,
+//       answer.projectDescription,
+//       answer.authorName,
+//       answer.gitHubHandle,
+//       answer.emailAddres
+
+// ## Description
+// ${data.projectDescription}.
+
+// ## Table of Contents
+
+// - [Installation](#installation)
+// - [Usage and test instructions](#usage)
+// - [License](#license)
+// - [How to contribute](#contribute)
+// - [Questions?](#questions)
+
+// ## Installation
+
+// If you are contributing to or extending an existing project, it is almost always easiest to continue using that project’s license.
+// ${data.installation}
+
+// ## Usage and test instructions
+
+// ${data.usage}
+
+// ## Licence
+
+// ${data.license}
+
+// ## How to Contribute
+
+// ${data.contribute}
+
+// ## Questions?
+// Do you have any queries? Please feel to contact me via my Github handle:${data.gitHubHandle}
+
+//`;
